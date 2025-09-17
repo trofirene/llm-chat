62b2527de68084daeeea078a113a300294a2f613
Ирина Трофимова <irinatrofimova@MacBook-Air-Irina.local>
2025-09-17 17:54:53 +0300

    llm прикручена, localStorage, история сообщений

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="container">
     <h1>How can I help you?</h1>
+    <div class="card response" v-if="AIresponse" v-for="message in messageHistory">
+      {{ message }}
+      <!-- <MessageForm /> -->
+    </div>
     <div class="card">
-      <input type="text" placeholder="Enter your message" />
+      <textarea type="text" placeholder="Enter your message" v-model="message"></textarea>
       <hr />
       <div class="btns-container">
         <button class="btn">
@@ -17,7 +21,7 @@
         <button class="btn">
           <img src="@/icons/Grid.svg" alt="grid" />
         </button>
-        <button class="btn btn-text">
+        <button class="btn btn-text" @click="sendMessage">
           <img src="@/icons/Send.svg" alt="send" />
           Send message
         </button>
@@ -27,8 +31,81 @@
 </template>
 
 <script>
+import { ref, onMounted } from 'vue'
+import MessageForm from './api/MessageForm.vue'
+
 export default {
-  name: 'MainPage',
+  components: { MessageForm },
+  setup() {
+    const message = ref('')
+    const AIresponse = ref('')
+    const messageHistory = ref([])
+
+    onMounted(() => {
+      if (localStorage.getItem('chatHistory') !== null) {
+        messageHistory.value = JSON.parse(localStorage.getItem('chatHistory'))
+      }
+    })
+
+    function saveMessage(text, sender) {
+      let chatHistory
+      let newMessage = {
+        text: text,
+        sender: sender,
+        timestamp: new Date().getTime(),
+      }
+
+      if (localStorage.getItem('chatHistory') === null) {
+        chatHistory = []
+        chatHistory.push(newMessage)
+        messageHistory.value.push(newMessage)
+        localStorage.setItem('chatHistory', JSON.stringify(chatHistory))
+      } else {
+        chatHistory = JSON.parse(localStorage.getItem('chatHistory'))
+        chatHistory.push(newMessage)
+        messageHistory.value.push(newMessage)
+        localStorage.setItem('chatHistory', JSON.stringify(chatHistory))
+      }
+    }
+
+    async function sendMessage() {
+      try {
+        saveMessage(message.value, 'user')
+        let response = await fetch('https://openrouter.ai/api/v1/chat/completions', {
+          method: 'POST',
+          headers: {
+            Authorization:
+              'Bearer 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',
+            'HTTP-Referer': 'http://localhost:5173',
+            'X-Title': 'My LLM Model>',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            model: 'deepseek/deepseek-chat-v3.1:free',
+            messages: [
+              {
+                role: 'user',
+                content: message.value,
+              },
+            ],
+          }),
+        })
+        let data = await response.json()
+        AIresponse.value = data.choices[0].message.content
+        saveMessage(AIresponse.value, 'ai')
+        message.value = ''
+      } catch (error) {
+        console.log('Error', error)
+      }
+    }
+
+    return {
+      sendMessage,
+      message,
+      AIresponse,
+      messageHistory,
+    }
+  },
 }
 </script>
 
---
+++ src/components/api/MessageForm.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card response">
+    <h1>chuuu</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.response {
+  background-color: lightgray;
+}
+</style>
